a2d5ee783dcc53b0d1c01680add910fc710f5141
krishnasai453 <krishnasai453@gmail.com>
2019-08-23 19:33:34 +0000

    Added functions to handle deck of cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,82 @@
-defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+qdefmodule Cards do
 
-  @doc """
-  Hello world.
+	@moduledoc """
+		Provides functions for handling deck of cards
+	"""
 
-  ## Examples
+	@doc """
+		Returns a list of playing cards
+	"""
+	def create_deck do
+		suits 	= ["Spades", "Clubs", "Hearts", "Diamonds"]
+		values 	= ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+		for suit<-suits, value<-values do
+			"#{value} of #{suit}"
+		end
+	end
 
-      iex> Cards.hello()
-      :world
+	@doc """
+		Shuffles a deck of playing cards
+	"""
+	def shuffle(deck) do
+		Enum.shuffle(deck)
+	end
 
-  """
-  def hello do
-    :world
-  end
+	@doc """
+		Divides the deck into hand and remaining cards in deck. Deals the card according to handsize provices
+
+		## Examples
+
+			iex> deck = Cards.create_deck
+			iex> Cards.deal(deck, 1)
+			["Ace of Spades"]
+
+	"""
+	def contains?(deck, card) do
+		Enum.member?(deck, card)
+	end
+
+	@doc """
+		Divides the deck into hand and remaining cards in deck. Deals the card according to handsize provices
+
+		## Examples
+
+			iex> deck = Cards.create_deck
+			iex> Cards.deal(deck, 1)
+			["Ace of Spades"]
+
+	"""
+
+	def  deal(deck, hand_size) do
+		{deal_cards, _rest} = Enum.split(deck, hand_size)
+		deal_cards
+	end
+
+	@doc """
+		Saves the deck/hand of cards to filename provided
+	"""
+	def save(deck, filename) do
+		binary = :erlang.term_to_binary(deck)
+		File.write(filename, binary)
+	end
+
+		@doc """
+			Loads the deck/hand of cards from filename provided
+		"""
+	def load(filename) do
+		case File.read!(filename) do
+			{:error, _reason} -> "The file doesnt exist"
+			{:ok, bin}     -> :erlang.binary_to_term(bin)
+		end
+	end
+
+		@doc """
+			Creates, Shuffles and returns the cards according to handsize
+		"""
+	def create_hand(hand_size) do
+		{hand, _rest}  = create_deck()
+										|> shuffle()
+										|> deal(hand_size)
+	  hand
+	end
 end
